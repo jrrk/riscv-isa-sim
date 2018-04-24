0f504fc1e7a481bf00b4f02bc74ea4e4c1966a17
Jonathan Richard Robert Kimmitt <jrrk2@cl.cam.ac.uk>
2018-04-24 19:29:52 +0100

    Start to implement ethernet functions

--- riscv/eth.cc
+++ riscv/eth.cc
@@ -16,10 +16,14 @@
 #include <signal.h>
 
 #include "devices.h"
+#include "eth.h"
+
+static uint32_t machi, maclo, txlen, txfcs, mdio, rxfcs, rsr, rxlen, reset;
 
 eth_device_t::eth_device_t()
 {
-
+  machi = 0x2301;
+  maclo = 0x00890702;
 }
 
 bool eth_device_t::load(reg_t addr, size_t len, uint8_t* bytes)
@@ -27,7 +31,19 @@ bool eth_device_t::load(reg_t addr, size_t len, uint8_t* bytes)
   int rslt, cnt;
   if (addr + len > 0x8000)
     return false;
-  memset(bytes, 0, len);
+
+  switch(addr)
+    {
+    case MACLO_OFFSET: printf("eth_load(MACLO_OFFSET);\n"); memcpy(bytes, &maclo, len); break;
+    case MACHI_OFFSET: printf("eth_load(MACHI_OFFSET);\n"); memcpy(bytes, &maclo, len); break;
+    case TPLR_OFFSET: printf("eth_load(TPLR_OFFSET);\n"); memcpy(bytes, &txlen, len); break;
+    case TFCS_OFFSET: printf("eth_load(TFCS_OFFSET);\n"); memcpy(bytes, &txfcs, len); break;
+    case MDIOCTRL_OFFSET: printf("eth_load(MDIOCTRL_OFFSET);\n"); memcpy(bytes, &mdio, len); break;
+    case RFCS_OFFSET: printf("eth_load(RFCS_OFFSET);\n"); memcpy(bytes, &rxfcs, len); break;
+    case RSR_OFFSET: printf("eth_load(RSR_OFFSET);\n"); memcpy(bytes, &rsr, len); break;
+    case RPLR_OFFSET: printf("eth_load(RPLR_OFFSET);\n"); memcpy(bytes, &rxlen, len); break;
+      
+    }
   return true;
 }
 
@@ -35,6 +51,19 @@ bool eth_device_t::store(reg_t addr, size_t len, const uint8_t* bytes)
 {
   if (addr + len > 0x8000)
     return false;
+
+  switch(addr)
+    {
+    case MACLO_OFFSET: printf("eth_store(MACLO_OFFSET);\n"); memcpy(&maclo, bytes, len); break;
+    case MACHI_OFFSET: printf("eth_store(MACHI_OFFSET);\n"); memcpy(&machi, bytes, len); break;
+    case TPLR_OFFSET: printf("eth_store(TPLR_OFFSET);\n"); memcpy(&txlen, bytes, len); break;
+    case TFCS_OFFSET: printf("eth_store(TFCS_OFFSET);\n"); /* cancel */ break;
+    case MDIOCTRL_OFFSET: printf("eth_store(MDIOCTRL_OFFSET);\n"); memcpy(&mdio, bytes, len); break;
+    case RFCS_OFFSET: printf("eth_store(RFCS_OFFSET);\n"); /* unused */ break;
+    case RSR_OFFSET: printf("eth_store(RSR_OFFSET);\n"); memcpy(&reset, bytes, len); break;
+    case RPLR_OFFSET: printf("eth_store(RPLR_OFFSET);\n"); /* unused */ break;
+      
+    }
 
   return true;
 }
